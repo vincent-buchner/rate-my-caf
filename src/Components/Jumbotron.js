68e89347cba96172ed5a60eb4ba2f694c326b6b9
Vincent Buchner <vincentbuchner01@gmail.com>
2022-03-26 21:27:01 -0500

    Update Jumbotron.js

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -2,12 +2,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 
 function Greetings() {
-    return(
+    return (
         <>
-            <Container fluid className='p-0 my-5' style={{width: "100%", height: "500px", backgroundColor: "#005a9c"}}>
+            <Container fluid className='p-0 my-5' style={{ width: "100%", height: "500px", backgroundColor: "#005a9c" }}>
                 <Row>
                     <Col className="d-flex justify-content-center align-items-center py-4 text-center">
-                        <h1 style={{fontFamily: "mont_bold", color: "white"}}>Find out what students think before you decide to eat there!</h1>
+                        <h1 style={{ fontFamily: "mont_bold", color: "white" }}>Find out what students think before you decide to eat there!</h1>
                     </Col>
                 </Row>
             </Container>
