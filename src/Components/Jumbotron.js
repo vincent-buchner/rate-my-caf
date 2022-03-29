7aa58b8c7b76175bfe2c967e11a9daacae389a11
Vincent Buchner <vincentbuchner01@gmail.com>
2022-03-28 21:08:43 -0500

    Downloaded icons package and removed the row
    TODO: Figure out why the row adds a margin/padding to itself

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -5,11 +5,24 @@ function Greetings() {
     return (
         <>
             <Container fluid className='p-0 my-5' style={{ width: "100%", height: "500px", backgroundColor: "#005a9c" }}>
-                <Row>
-                    <Col className="d-flex justify-content-center align-items-center py-4 text-center">
+                
+                    <Col className="d-flex flex-column justify-content-center align-items-center py-4 text-center">
                         <h1 style={{ fontFamily: "mont_bold", color: "white" }}>Find out what students think before you decide to eat there!</h1>
+
+                        <Col>
+                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
+                            </div>
+                        </Col>
                     </Col>
-                </Row>
+                
             </Container>
         </>
     );
