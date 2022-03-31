1384e8a6f9c93cfd3b700215b7ecf5307701f108
Vincent Buchner <vincentbuchner01@gmail.com>
2022-03-30 22:27:46 -0500

    Update Jumbotron.js

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -4,25 +4,26 @@ import '../App.css';
 function Greetings() {
     return (
         <>
-            <Container fluid className='p-0 my-5' style={{ width: "100%", height: "500px", backgroundColor: "#005a9c" }}>
-                
+            <Container fluid className='p-0 my-5' style={{ width: "100%", height: "600px", backgroundColor: "#005a9c" }}>
+                <Row style={{ margin: "0px" }}>
                     <Col className="d-flex flex-column justify-content-center align-items-center py-4 text-center">
                         <h1 style={{ fontFamily: "mont_bold", color: "white" }}>Find out what students think before you decide to eat there!</h1>
-
-                        <Col>
-                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{ width: "100px", height: "100px", backgroundColor: "white", borderRadius: "50px" }}>
-                            </div>
-                        </Col>
+                        <div style={{width: "100%"}}className='d-flex flex-row justify-content-around align-items-center py-5'>
+                            <Col md={4} className='d-flex justify-content-center'>
+                                <div style={{ width: "200px", height: "200px", backgroundColor: "white", borderRadius: "100px" }}>
+                                </div>
+                            </Col>
+                            <Col md={4} className='d-flex justify-content-center'>
+                                <div style={{ width: "200px", height: "200px", backgroundColor: "white", borderRadius: "100px" }}>
+                                </div>
+                            </Col>
+                            <Col md={4} className='d-flex justify-content-center'>
+                                <div style={{ width: "200px", height: "200px", backgroundColor: "white", borderRadius: "100px" }}>
+                                </div>
+                            </Col>
+                        </div>
                     </Col>
-                
+                </Row>
             </Container>
         </>
     );
