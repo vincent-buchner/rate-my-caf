3f6009673f2b73b320c7f067e3d2873cda0acb95
Vincent Buchner <vincentbuchner01@gmail.com>
2022-04-09 15:07:35 -0500

    text center

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -8,7 +8,7 @@ function Quote() {
                 <Row>
                     <div className='d-flex flex-column justify-content-center align-items-center my-5'>
                         <div className='display-6' style={{fontFamily: "mont_bold"}}>Wanna See More?</div>
-                        <p>Send In Your Suggestions via Email: <u>ratemycaf.suggestions@gmail.com</u></p>
+                        <p className='text-center'>Send In Your Suggestions via Email: <u>ratemycaf.suggestions@gmail.com</u></p>
                         <p>Click <a href='https://github.com/vincent-buchner/rate-my-caf' target="_blank">Here</a> for Github Repo</p>
                     </div>
                 </Row>
