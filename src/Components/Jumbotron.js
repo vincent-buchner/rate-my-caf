8a8b1fa2d278304d6e38d318ee97dd36e1ccb51b
Vincent Buchner <vincentbuchner01@gmail.com>
2022-04-30 15:06:47 -0500

    Replaced Meta Tags, Added Worker Ad

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -21,7 +21,7 @@ function Greetings() {
                         <img 
                             src='https://images.unsplash.com/photo-1529973565457-a60a2ccf750d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
                             className='img-fluid p-5'
-                            alt='Burger!'
+                            alt='A man eating a burger at a cafeteria.'
                             style={{borderRadius: "60px"}}
                         />
                     </Col>
---
+++ src/Components/MeetWorker.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MeetWorker({name, img}) {
+    return(
+        <>
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        </>
+    );
+};
+
+export default MeetWorker;
